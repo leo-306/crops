050199ca289f6e78419f347c1479cf8dc8138ba3
liulei11 <liulei11@kuaishou.com>
2022-09-08 14:57:23 +0800

    feat: 修改 demo

--- demo/app.tsx
+++ demo/app.tsx
@@ -1,26 +1,48 @@
 import React, { FC, useRef, useState } from 'react';
 import ReactDom from 'react-dom';
 import MultipleCorpSketch from '../src';
-import { AnyType } from '../src/types';
 import { identity } from 'lodash';
 
 const App: FC = () => {
-	const [coordinates, setCoordinates] = useState([]);
+	const [coordinates, setCoordinates] = useState([
+		{
+			x: 161,
+			y: 589,
+			width: 231,
+			height: 113,
+			id: 'u_vFRE6',
+		}
+	]);
 	const ref = useRef({ getCropData: identity });
+
+	const getCorpData = () => {
+		const cropData = ref.current.getCropData(coordinates);
+		setCoordinates(cropData);
+		console.log('图片数据: ', cropData);
+	};
 	return (
 		<>
 			<MultipleCorpSketch
 				coordinates={coordinates}
 				width={544}
-				// src="https://js-ec.static.yximgs.com/udata/pkg/ks-merchant/cps-hybrid/empty_position.9b16b85c5a152402.png"
-				src="https://ali2.a.kwimgs.com/kos/nlav11092/fangzhou/pub/compress/image-55e7ab12-7fcf-46ec-9e4f-1702083cedb4.png"
+				src="https://img.zcool.cn/community/016dbe57b021070000012e7eedcf18.jpg@2o.jpg"
 				onChange={(_, _index, coordinates) => {
 					setCoordinates(coordinates);
 				}}
 				ref={ref}
 			/>
 			<div style={{ margin: '20px' }}>
-				<button onClick={() => console.log(ref.current.getCropData(coordinates))}>获取图片数据</button>
+				<button onClick={getCorpData}>获取图片数据</button>
+			</div>
+			<div>
+				{coordinates.map((coordinate, index) => {
+					return coordinate.img ? (
+						<div key={coordinate.id}>
+							<h5>热区{index + 1} ID: {coordinate.id}</h5>
+							<img style={{ width: coordinate.width, height: coordinate.height }} src={coordinate.img} alt=""/>
+						</div>
+					) : null;
+				})}
 			</div>
 		</>
 	);
