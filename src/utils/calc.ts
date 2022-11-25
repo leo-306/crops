dee9d4311bf45010137d498a43ba00ecfeb077d3
leo306 <liulei.leo306@gmail.com>
2022-11-25 11:24:58 +0800

    feat: 弧度计算注释

--- src/utils/calc.ts
+++ src/utils/calc.ts
@@ -5,7 +5,7 @@ export const calcRadians = (point: Point, origin: Point): number => {
 	return Math.atan2(point.x - origin.x, point.y - origin.y);
 };
 
-/** 数学角度转换为浏览器识别的角度 */
+/** 数学角度(弧度)转换为浏览器识别的角度 */
 export const convertRadiansToDegrees = (radians: number) => {
 	/** 代表向左上滑动 radians 为负数，* -1 得到最终的偏移值 */
 	return (radians * (180 / Math.PI) * -1) + 180;
